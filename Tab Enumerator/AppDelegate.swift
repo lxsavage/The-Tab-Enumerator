d641e08ca1e9b56f634ea0b7cd386018e6969a09
lxsavage <31578557+lxsavage@users.noreply.github.com>
2026-05-11 13:32:05 -0400

    Initial Commit

---
+++ Tab Enumerator/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Tab Enumerator
+//
+//  Created by Logan Savage on 2026.05.11.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
