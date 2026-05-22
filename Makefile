0b23f0d26c17a54025b4fd45522b45f6fcfbc22e
lxsavage <31578557+lxsavage@users.noreply.github.com>
2026-05-22 15:24:45 -0400

    Fixed broken functionality in browsers that don't support the browser namespace, added editorconfig to facilitate non-Xcode development

---
+++ Makefile
@@ -0,0 +1,3 @@
+# Added to make building MV3 extension easier
+all:
+	@$(MAKE) -C "Chrome Extension"
--- Tab Enumerator Extension/Resources/popup.js
+++ Tab Enumerator Extension/Resources/popup.js
@@ -1,27 +1,28 @@
 const isMacOS = window.navigator.userAgentData
-    ? window.navigator.userAgentData.platform === 'macOS'
+    ? window.navigator.userAgentData.platform === "macOS"
     : /Mac/i.test(window.navigator.userAgent);
 
-const manifest = browser.runtime.getManifest();
+const manifest = chrome.runtime.getManifest();
 
 function setAttr(attr) {
     if (!manifest[attr]) throw new Error(`manifest attribute ${attr} not set!`);
 
-    const $attr = document.getElementById('tte-' + attr);
+    const $attr = document.getElementById("tte-" + attr);
     if (!$attr) throw new Error(`element #tte-${attr} not found!`);
 
     $attr.innerHTML = manifest[attr];
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    setAttr('version');
-    setAttr('name');
+document.addEventListener("DOMContentLoaded", () => {
+    setAttr("version");
+    setAttr("name");
 
     if (!isMacOS) return;
 
     // SHIM: MacOS has the tab jump shortcut modifier as meta instead of ctrl
-    document.getElementById('modifier').innerHTML = '&#8984;'; // CMD symbol
-})
+    document.getElementById("modifier").innerHTML = "&#8984;"; // CMD symbol
+});
 
-document.getElementById("settings-button")
-    .addEventListener('click', () => chrome.runtime.openOptionsPage());
+document
+    .getElementById("settings-button")
+    .addEventListener("click", () => chrome.runtime.openOptionsPage());
