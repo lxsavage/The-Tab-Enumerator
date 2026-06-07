ea14751131673462d693750b0566f298f97944da
lxsavage <31578557+lxsavage@users.noreply.github.com>
2026-06-07 14:33:43 -0400

    Added settings reset button, minor cleanup

--- Tab Enumerator Extension/Resources/options.js
+++ Tab Enumerator Extension/Resources/options.js
@@ -1,18 +1,21 @@
-const isSafari = navigator.vendor &&
-    navigator.vendor.indexOf('Apple') > -1 &&
+const isSafari =
+    navigator.vendor &&
+    navigator.vendor.indexOf("Apple") > -1 &&
     navigator.userAgent &&
-    navigator.userAgent.indexOf('CriOS') == -1 &&
-    navigator.userAgent.indexOf('FxiOS') == -1;
+    navigator.userAgent.indexOf("CriOS") == -1 &&
+    navigator.userAgent.indexOf("FxiOS") == -1;
 
 function jsonifyForm(id) {
-    const $formInputs = document.querySelectorAll(`form#${id} input:not([type='submit'])`);
+    const $formInputs = document.querySelectorAll(
+        `form#${id} input:not([type='submit'])`,
+    );
 
     const result = {};
     for (const $input of $formInputs) {
         if (!$input.name) continue;
 
         switch ($input.type) {
-            case 'checkbox':
+            case "checkbox":
                 result[$input.name] = $input.checked;
                 break;
             default:
@@ -29,64 +32,100 @@ async function loadSettingsAsync() {
     try {
         settings = await chrome.storage.sync.get(null);
     } catch (ex) {
-        console.error('failed to load preexisting settings from sync store: ', ex);
+        console.error(
+            "failed to load preexisting settings from sync store: ",
+            ex,
+        );
         return;
     }
 
     try {
-        const $formInputs = document.querySelectorAll("form#settings-form input:not([type='submit'])");
+        const $formInputs = document.querySelectorAll(
+            "form#settings-form input:not([type='submit'])",
+        );
+        const $reset = document.getElementById("settings-form-reset");
+        const $submit = document.querySelector(
+            "form#settings-form input[type='submit']",
+        );
+
         for (const $input of $formInputs) {
             if (!($input.name in settings)) continue;
 
-            if ($input.type === 'checkbox') {
+            if ($input.type === "checkbox") {
                 $input.checked = settings[$input.name];
             } else {
                 $input.value = settings[$input.name];
             }
+
+            $input.addEventListener("change", (_) => {
+                $reset.removeAttribute("disabled");
+                $submit.removeAttribute("disabled");
+            });
         }
     } catch (ex) {
-        console.error('failed to apply preexisting settings from sync store: ', ex);
+        console.error(
+            "failed to apply preexisting settings from sync store: ",
+            ex,
+        );
     }
 }
 
 async function syncSettingsAsync() {
-    const form = jsonifyForm('settings-form');
-    
+    const form = jsonifyForm("settings-form");
+
     try {
         await chrome.storage.sync.set(form);
-        console.debug('wrote settings to sync store');
+        console.debug("wrote settings to sync store");
     } catch (ex) {
-        console.error('failed to write settings to sync store: ', ex);
+        console.error("failed to write settings to sync store: ", ex);
     }
 }
 
 loadSettingsAsync().then(() => {
-    const $form = document.getElementById('settings-form');
-    $form.addEventListener('submit', async evt => {
+    const $form = document.getElementById("settings-form");
+    const $reset = document.getElementById("settings-form-reset");
+    const $submit = document.querySelector(
+        '#settings-form input[type="submit"]',
+    );
+
+    $reset.addEventListener("click", async (_) => {
+        await loadSettingsAsync();
+        $reset.setAttribute("disabled", "disabled");
+        $submit.setAttribute("disabled", "disabled");
+    });
+
+    $form.addEventListener("submit", async (evt) => {
         evt.preventDefault();
         await syncSettingsAsync();
         await loadSettingsAsync();
-        alert('Settings saved.');
+        alert("Settings saved.");
+
+        $reset.setAttribute("disabled", "disabled");
+        $submit.setAttribute("disabled", "disabled");
         return false;
     });
-    $form.removeAttribute('hidden');
 
-    document.getElementById('settings-form-load')
-        .setAttribute('hidden', 'hidden');
+    document
+        .getElementById("settings-form-load")
+        .setAttribute("hidden", "hidden");
+
+    $form.removeAttribute("hidden");
+    $reset.setAttribute("disabled", "disabled");
+    $submit.setAttribute("disabled", "disabled");
 });
 
 if (isSafari) {
-    for (const elem of document.getElementsByClassName('not-safari')) {
+    for (const elem of document.getElementsByClassName("not-safari")) {
         elem.remove();
     }
-    for (const elem of document.getElementsByClassName('safari-only')) {
-        elem.removeAttribute('hidden');
+    for (const elem of document.getElementsByClassName("safari-only")) {
+        elem.removeAttribute("hidden");
     }
 } else {
-    for (const elem of document.getElementsByClassName('safari-only')) {
+    for (const elem of document.getElementsByClassName("safari-only")) {
         elem.remove();
     }
-    for (const elem of document.getElementsByClassName('not-safari')) {
-        elem.removeAttribute('hidden');
+    for (const elem of document.getElementsByClassName("not-safari")) {
+        elem.removeAttribute("hidden");
     }
 }
